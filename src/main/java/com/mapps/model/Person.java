8110d4da6b8413218461c17113b7519ea9fc294d
martinbomio <martinbomio@gmail.com>
2013-11-19 16:38:38 -0200

    adding files to project

---
+++ src/main/java/com/mapps/model/Person.java
@@ -0,0 +1,59 @@
+package com.mapps.model;
+
+import java.util.Date;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * Representation of a person to the system. Abstraction for reuse.
+ */
+@MappedSuperclass
+public abstract class Person{
+    protected String name;
+    protected String lastName;
+    @Temporal(TemporalType.DATE)
+    protected Date birth;
+    protected String gender;
+    protected String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/mapps/model/User.java
@@ -0,0 +1,72 @@
+package com.mapps.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Parameter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.jasypt.hibernate.type.EncryptedStringType;
+
+/**
+ * Representation of the User of the system
+ */
+@Entity
+@Table(name = "Users" )
+@TypeDef(
+        name="encryptedString",
+        typeClass=EncryptedStringType.class,
+        parameters= {
+                @Parameter(name="encryptorRegisteredName", value="myHibernateStringEncryptor")
+        }
+)
+public class User extends Person{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    Long id;
+    private String userName;
+    @Type(type="encryptedString")
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String lastName, Date birth, String gender,
+                String email, String userName, String password){
+        this.name = name;
+        this.lastName = lastName;
+        this.birth = birth;
+        this.gender = gender;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
